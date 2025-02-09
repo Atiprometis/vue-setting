3eed35bc7c149eb48b8b125f49f772cd49ef9759
Fang <fang.live-@msn.com>
2025-02-09 23:43:02 +0700

    add axios add dotenv add pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,16 @@
 </template>
 
 <script setup>
+import axios from 'axios';
+
+axios.get('https://api.example.com/data')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
 
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from '@/router'
-import store from './store'
+import store from './stores'
+
 
 import BootstrapVue3 from 'bootstrap-vue-3';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
 const app = createApp(App);
+app.use(createPinia());
 app.use(store);
 app.use(BootstrapVue3);
 app.use(router);
